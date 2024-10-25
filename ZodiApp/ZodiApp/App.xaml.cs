d4b3eac3de304b6715462639cc560f7ca6e3c67d
RJK416 <gegaoragvelidze97@gmail.com>
2024-10-25 22:22:39 +0400

    Starting new project

---
+++ ZodiApp/ZodiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZodiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
